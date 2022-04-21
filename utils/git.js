904b81f17bfcf28ed9aae91b1e7703e807e1a533
Eric Anderson <anderson.eric.t@gmail.com>
2022-04-21 10:14:43 -0400

    todo: updated

--- utils/git.js
+++ utils/git.js
@@ -86,8 +86,7 @@ export default async function git() {
 			const answer = await inquirer.prompt(question);
 			if (answer.pushConfirm == 'y') {
 				spinner.start('Pushing to remote repository');
-				//exec(`git push https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USERNAME}/${path.basename(path.resolve(process.cwd()))}.git`, (error, stdout, stderr) => {
-				exec(`git push `, (error, stdout, stderr) => {
+				exec(`git push https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USERNAME}/${path.basename(path.resolve(process.cwd()))}.git`, (error, stdout, stderr) => {
 					if (error) {
 						spinner.fail(`error: ${error.message}`);
 						return;
