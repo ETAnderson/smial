b7c1bd5b7ca7b4a302e6f9ae2ee5bec0c7e69a36
Eric Anderson <anderson.eric.t@gmail.com>
2022-04-27 11:47:31 -0400

    feat: finished building out cli todo menu

--- utils/todo.js
+++ utils/todo.js
@@ -1,4 +1,4 @@
-import fs from 'path';
+import fs from 'fs';
 import chalk from 'chalk';
 import ora from 'ora';
 import inquirer from 'inquirer';
@@ -7,8 +7,27 @@ import { fileURLToPath } from 'url';
 
 export default async function todo() {
 	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = dirname(__filename);
+	const __dirname = dirname(__filename).slice(0, -5) + 'todo/';
 	const spinner = ora();
+	const displayTodoList = () => {
+		let data = [];
+
+		const fileData = fs.readFileSync(__dirname + 'todo.txt').toString();
+		data = fileData.split('\n');
+
+		let filterData = data.filter(function (value) {
+			return value !== '';
+		});
+
+		if (filterData.length === 0) {
+			console.log('There are no pending todos!');
+		}
+
+		for (let i = 0; i < filterData.length; i++) {
+			console.log(filterData.length - i + '. ' + filterData[i]);
+		}
+	};
+
 	const question = [
 		{
 			name: 'todoMenu',
@@ -21,39 +40,21 @@ export default async function todo() {
 	const answer = await inquirer.prompt(question);
 
 	if (answer.todoMenu == 'start') {
+		console.log(__dirname);
 		if (fs.existsSync(__dirname + 'todo.txt') === false) {
 			spinner.start('building todo.txt....');
-			let createStream = fs.createWriteStream('todo.txt');
+			let createStream = fs.createWriteStream(__dirname + 'todo.txt');
 			createStream.end();
-			spinner.end('todo.txt built!');
+			spinner.succeed('todo.txt built!');
 		}
 
 		if (fs.existsSync(__dirname + 'done.txt') === false) {
 			spinner.start('building done.txt...');
-			let createStream = fs.createWriteStream('done.txt');
+			let createStream = fs.createWriteStream(__dirname + 'done.txt');
 			createStream.end();
-			spinner.end('done.txt built!');
+			spinner.succeed('done.txt built!');
 		}
 	} else if (answer.todoMenu == 'list') {
-		const displayTodoList = () => {
-			let data = [];
-
-			const fileData = fs.readFileSync(__dirname + 'todo.txt').toString;
-			data = fileData.split('\n');
-
-			let filterData = data.filter(function (value) {
-				return value !== '';
-			});
-
-			if (filterData.length === 0) {
-				console.log('There are no pending todos!');
-			}
-
-			for (let i = 0; i < filterData.length; i++) {
-				console.log(filterData.length - i + '. ' + filterData[i]);
-			}
-		};
-
 		displayTodoList();
 	} else if (answer.todoMenu == 'add') {
 		const question = [
@@ -69,7 +70,7 @@ export default async function todo() {
 		const newTask = answer.addMenu;
 
 		let data = [];
-		const fileData = fs.readFileSync(__dirname + 'todo.txt').toString;
+		const fileData = fs.readFileSync(__dirname + 'todo.txt').toString();
 
 		fs.writeFile(
 			__dirname + 'todo.txt',
@@ -93,9 +94,10 @@ export default async function todo() {
 		const answer = await inquirer.prompt(question);
 
 		let data = [];
-		const deleteIndex = answer.deleteMenu - 1;
 
-		const fileData = fs.readFileSync(__dirname + 'todo.txt').toString;
+		const deleteIndex = parseInt(answer.deleteMenu, 10);
+
+		const fileData = fs.readFileSync(__dirname + 'todo.txt').toString();
 
 		data = fileData.split('\n');
 
@@ -110,7 +112,18 @@ export default async function todo() {
 					' does not exist. Nothing deleted.'
 			);
 		} else {
-			console.log('Error: Missing NUMBER for deleting todo.');
+			filterData.splice(filterData.length - deleteIndex, 1);
+
+			// Join the array to form a string
+			const newData = filterData.join('\n');
+
+			// Write the new data back in file
+			fs.writeFile(__dirname + 'todo.txt', newData, function (err) {
+				if (err) throw err;
+
+				// Logs the deleted index
+				console.log('Deleted todo #' + deleteIndex);
+			});
 		}
 	} else if (answer.todoMenu == 'complete') {
 		displayTodoList();
@@ -124,7 +137,7 @@ export default async function todo() {
 
 		const answer = await inquirer.prompt(question);
 
-		const completeIndex = answer.completeMenu - 1;
+		const completeIndex = parseInt(answer.completeMenu, 10);
 		if (completeIndex) {
 			let data = [];
 
@@ -133,12 +146,10 @@ export default async function todo() {
 			let dateString = dateobj.toISOString().substring(0, 10);
 
 			const fileData = fs
-				.readFileSync(currentWorkingDirectory + 'todo.txt')
+				.readFileSync(__dirname + '/todo.txt')
 				.toString();
 
-			const doneData = fs
-				.readFileSync(currentWorkingDirectory + 'done.txt')
-				.toString();
+			const doneData = fs.readFileSync(__dirname + 'done.txt').toString();
 
 			data = fileData.split('\n');
 
@@ -146,30 +157,30 @@ export default async function todo() {
 				return value !== '';
 			});
 
-			if (doneIndex > filterData.length || doneIndex <= 0) {
-				console.log('Error: todo #' + doneIndex + ' does not exist.');
+			if (completeIndex > filterData.length || completeIndex <= 0) {
+				console.log(
+					'Error: todo #' + completeIndex + ' does not exist.'
+				);
 			} else {
 				const deleted = filterData.splice(
-					filterData.length - doneIndex,
+					filterData.length - completeIndex,
 					1
 				);
 
 				const newData = filterData.join('\n');
 
-				fs.writeFile(
-					currentWorkingDirectory + 'todo.txt',
-					newData,
-					function (err) {
-						if (err) throw err;
-					}
-				);
+				fs.writeFile(__dirname + 'todo.txt', newData, function (err) {
+					if (err) throw err;
+				});
 
 				fs.writeFile(
-					currentWorkingDirectory + 'done.txt',
+					__dirname + 'done.txt',
 					'x ' + dateString + ' ' + deleted + '\n' + doneData,
 					function (err) {
 						if (err) throw err;
-						console.log('Marked todo #' + doneIndex + ' as done.');
+						console.log(
+							'Marked todo #' + completeIndex + ' as done.'
+						);
 					}
 				);
 			}
@@ -185,12 +196,8 @@ export default async function todo() {
 
 		let dateString = dateobj.toISOString().substring(0, 10);
 
-		const todo = fs
-			.readFileSync(currentWorkingDirectory + 'todo.txt')
-			.toString();
-		const done = fs
-			.readFileSync(currentWorkingDirectory + 'done.txt')
-			.toString();
+		const todo = fs.readFileSync(__dirname + 'todo.txt').toString();
+		const done = fs.readFileSync(__dirname + 'done.txt').toString();
 
 		todoData = todo.split('\n');
 
